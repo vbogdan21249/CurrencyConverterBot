c019c815ad4a4060ff390c6e700bd8c0e82ae032
vbogdan21249 <6794117@stud.nau.edu.ua>
2024-04-17 20:38:29 +0300

    initial commit

---
+++ src/main/java/io/vb2/CurrencyConverterBot/CurrencyConverterBotApplication.java
@@ -0,0 +1,13 @@
+package io.vb2.CurrencyConverterBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyConverterBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurrencyConverterBotApplication.class, args);
+    }
+
+}
