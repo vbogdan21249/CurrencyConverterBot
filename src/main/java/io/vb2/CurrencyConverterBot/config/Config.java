79469597d33747833f99511f49ba97685cb99fd9
vbogdan21249 <6794117@stud.nau.edu.ua>
2024-04-27 15:51:32 +0300

    bot can only convert uah to dollars (!)

--- src/main/java/io/vb2/CurrencyConverterBot/config/Config.java
+++ src/main/java/io/vb2/CurrencyConverterBot/config/Config.java
@@ -1,13 +1,13 @@
 package io.vb2.CurrencyConverterBot.config;
 
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Data
+@Configuration
 public class Config {
+
+    @Value("${key.currencyConverterApi}")
     private String currencyConverterApiApiKey;
-
-    public String getCurrencyConverterApiApiKey() {
-        return currencyConverterApiApiKey;
-    }
-    public void setCurrencyConverterApiApiKey(String currencyConverterApiApiKey) {
-        this.currencyConverterApiApiKey = currencyConverterApiApiKey;
-    }
-
 }
