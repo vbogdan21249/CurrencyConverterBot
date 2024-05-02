b5590c264755fe967bdc8726644ed117cf13feb6
vbogdan21249 <6794117@stud.nau.edu.ua>
2024-05-02 19:59:32 +0300

    getting of key.CurrencyConverterApi fixed

--- src/main/java/io/vb2/CurrencyConverterBot/config/ApiConfig.java
+++ src/main/java/io/vb2/CurrencyConverterBot/config/ApiConfig.java
@@ -1,13 +1,14 @@
 package io.vb2.CurrencyConverterBot.config;
 
 import lombok.Data;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
-@Data
 @Configuration
-public class Config {
+@Data
 
+public class ApiConfig {
     @Value("${key.currencyConverterApi}")
-    private String currencyConverterApiApiKey;
+    String currencyConverterApiApiKey;
 }
--- src/main/java/io/vb2/CurrencyConverterBot/service/Converter.java
+++ src/main/java/io/vb2/CurrencyConverterBot/service/Converter.java
@@ -1,18 +1,21 @@
 package io.vb2.CurrencyConverterBot.service;
 
-import io.vb2.CurrencyConverterBot.config.Config;
+
 import io.vb2.CurrencyConverterBot.enums.Currency;
 import io.vb2.CurrencyConverterBot.source.ConverterSource;
-import io.vb2.CurrencyConverterBot.source.NBUSource;
+
+import lombok.Getter;
 import lombok.Setter;
-import models.CurrencyRate;
+import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.math.BigDecimal;
 
+@Component
 public class Converter {
+    @Getter
     @Setter
-    private static ConverterSource converterSource = new NBUSource();
+    private static ConverterSource converterSource;
 
     public static BigDecimal convert(Currency from, Currency to) throws IOException {
         return converterSource.rate(from, to);
