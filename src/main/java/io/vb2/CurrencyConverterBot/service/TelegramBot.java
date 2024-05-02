1e559690693ab1cb03ac131a2de6f82e90cfd92a
vbogdan21249 <6794117@stud.nau.edu.ua>
2024-05-03 02:14:51 +0300

    menu updated

--- src/main/java/io/vb2/CurrencyConverterBot/service/TelegramBot.java
+++ src/main/java/io/vb2/CurrencyConverterBot/service/TelegramBot.java
@@ -3,7 +3,9 @@ package io.vb2.CurrencyConverterBot.service;
 import io.vb2.CurrencyConverterBot.config.ApiConfig;
 import io.vb2.CurrencyConverterBot.config.BotConfig;
 
+import io.vb2.CurrencyConverterBot.exception.CurrencyConverterException;
 import io.vb2.CurrencyConverterBot.source.CurrencyConverterApiSource;
+import io.vb2.CurrencyConverterBot.source.NBUSource;
 import lombok.AllArgsConstructor;
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Component;
@@ -14,12 +16,13 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.util.Locale;
 
 @Component
 @AllArgsConstructor
 public class TelegramBot extends TelegramLongPollingBot {
     private static final Logger log = Logger.getLogger(TelegramBot.class);
-    
+
     private final ApiConfig apiConfig;
     private final BotConfig config;
     private final CurrencyManager currencyManager;
@@ -39,31 +42,81 @@ public class TelegramBot extends TelegramLongPollingBot {
         if (update.hasMessage() && update.getMessage().hasText()) {
             long chatId = update.getMessage().getChatId();
             String messageText = update.getMessage().getText();
-
+            // SETTING DEFAULT CURRENCY SOURCE
             if (Converter.getConverterSource() == null) {
-                Converter.setConverterSource(new CurrencyConverterApiSource(apiConfig));
+                Converter.setConverterSource(new NBUSource());
             }
 
-            if (messageText.equals("/start")) {
-                sendMessage(chatId, "Welcome to Currency Converter Bot!");
-                return;
+            // START COMMAND
+
+            switch (messageText) {
+                case "/start":
+                    sendMessage(chatId, "Welcome to Currency Converter Bot! /help for more information.");
+                    return;
+                case "/help":
+                    sendMessage(chatId, "Commands:\n" +
+                            "/start - start the bot\n" +
+                            "/changeSource - change currency source\n" +
+                            "Enter two currencies to change them (e.g. USD UAH)\n" +
+                            "Enter a number to convert it to the target currency\n");
+                    return;
+                case "/changeSource":
+                    if (Converter.getConverterSource() instanceof CurrencyConverterApiSource) {
+                        Converter.setConverterSource(new NBUSource());
+                        currencyManager.updateConverter("UAH");
+                        sendMessage(chatId, "NBU source has been set.\n" +
+                                "It always has UAH as a target currency.\n" +
+                                "Now you can convert " + currencyManager.getBaseCurrency() + " to " + currencyManager.getTargetCurrency() + "!");
+                    } else {
+                        Converter.setConverterSource(new CurrencyConverterApiSource(apiConfig));
+                        sendMessage(chatId, "CurrencyConverterAPI source has been set.\n" +
+                                "Now you can convert " + currencyManager.getBaseCurrency() + " to " + currencyManager.getTargetCurrency() + "!");
+
+                    }
+                    return;
+                default:
+                    // CONVERTING CURRENCY
+                    if (messageText.matches("-?\\d+(\\.\\d+)?")) {
+                        BigDecimal amount = new BigDecimal(messageText);
+                        if (amount.compareTo(BigDecimal.ZERO) < 0) {
+                            sendMessage(chatId, "Input cannot be negative.");
+                        } else {
+                            try {
+                                sendMessage(chatId, messageText + " " +
+                                        currencyManager.getBaseCurrency() + "\n" +
+                                        currencyManager.convert(amount).toString()
+                                        + " " + currencyManager.getTargetCurrency());
+                            } catch (CurrencyConverterException e) {
+                                sendMessage(chatId, e.getMessage());
+                                log.error(e.getMessage());
+                            } catch (IOException e) {
+                                sendMessage(chatId, "Error occurred while converting currency.");
+                                log.error(e.getMessage());
+                                throw new RuntimeException(e);
+                            }
+
+                        }
+                    }
+                    break;
             }
 
+            // CHANGING CURRENCIES
             String[] currencies = messageText.trim().split("\\s+");
             if (currencies.length == 2 && currencies[0].matches("\\b[a-zA-Z]{3,4}\\b") && currencies[1].matches("\\b[a-zA-Z]{3,4}\\b")) {
-                String baseCurrency = currencies[0];
-                String targetCurrency = currencies[1];
-                if (currencyManager.updateConverter(baseCurrency, targetCurrency)) {
-                    sendMessage(chatId, "Currencies has been successfully changed.");
-                } else {
-                    sendMessage(chatId, "Currencies has not been changed.");
+
+                String baseCurrency = currencies[0].toUpperCase();
+                String targetCurrency = currencies[1].toUpperCase();
+
+                if (Converter.getConverterSource() instanceof NBUSource) {
+                    sendMessage(chatId, "NBU source always has UAH as a target currency.");
+                    targetCurrency = "UAH";
                 }
-            } else {
                 try {
-                    sendMessage(chatId, currencyManager.convert(new BigDecimal(messageText)).toString());
-                } catch (IOException e) {
-                    log.error("Error occurred: " + e.getMessage());
-                    throw new RuntimeException(e);
+                    currencyManager.updateConverter(baseCurrency, targetCurrency);
+                    sendMessage(chatId, "Currencies has been successfully changed.\n" +
+                            "Now you can convert " + baseCurrency + " to " + targetCurrency + "!");
+                } catch (CurrencyConverterException e) {
+                    sendMessage(chatId, "Currencies has not been changed. " + e.getMessage());
                 }
             }
         }
@@ -77,7 +130,8 @@ public class TelegramBot extends TelegramLongPollingBot {
         try {
             execute(message);
         } catch (TelegramApiException e) {
-            log.error("Error occurred: " + e.getMessage());
+            sendMessage(chatId, e.getMessage());
+            log.error(e.getMessage());
         }
     }
 }
